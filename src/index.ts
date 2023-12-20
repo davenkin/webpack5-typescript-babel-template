25dfaf6d0fdb5c6bad12fc94ec06f283b093a4e0
Teng Yun <davenkin@126.com>
2023-12-20 20:47:18 +0800

    added ts interface demo

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,16 @@ import './styles/global.scss';
 import './index.scss';
 import {fromEvent} from 'rxjs';
 
+interface HelloWorld {
+    hello: string,
+    world: string,
+}
+
 const source = fromEvent(document.getElementById('theButton')!, 'click');
-source.subscribe(event => alert(`Button clicked at ${event.timeStamp}`));
+source.subscribe(event => {
+    let helloWorld: HelloWorld = {
+        world: "World",
+        hello: "Hello"
+    }
+    alert(`Button clicked at ${event.timeStamp} and say ${helloWorld.hello} ${helloWorld.world}`);
+});
